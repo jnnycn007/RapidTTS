ea535b1c567b0a335a2071008151f145cb855200
SWHL <liekkaskono@163.com>
2026-05-12 20:23:27 +0800

    docs: update README

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from rapidtts import RapidTTS, SynthesisRequest
 
 tts = RapidTTS()
 
-text = "今天是2026-05-08，猪肉价格是￥13.5，增长6.3%。车牌号码：京A86F29。"
+text = "2026年5月8日，猪肉价格为每斤13.5元，较前期上涨6.3%；车牌号码为京A86F29。"
 result = tts.synthesize(SynthesisRequest(text=text, voice="zm_010"))
 
 save_path = "rapidtts_result.wav"
